b074e6aaa22ff2fbf04ae9f9b428109ea1821462
Ian <bontaq@gmail.com>
2023-07-29 10:24:31 -0400

    The first step for the website

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Prelude hiding (div)
+
+import Purview
+
+topLevel = div
+  [ text "Purview" ]
+
+router _ = topLevel
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = serve defaultConfiguration router
