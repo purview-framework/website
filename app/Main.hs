416f5a70a7c8fb985ae5b3236acb44a1ec1b52cd
Ian <bontaq@gmail.com>
2023-07-30 10:27:31 -0400

    Maybe writing websites in haskell is fun.
    Like, a lot of fun?  I love that alternate colors

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import Prelude hiding (div)
+import Prelude hiding (div, span)
 import Text.RawString.QQ (r)
 
 import Purview
@@ -10,36 +10,47 @@ navStyle = [style|
   width: 100%;
   min-height: 50px;
   padding-top: 25px;
+  display: flex;
+  justify-content: space-around;
 |]
 
 aStyle = [style|
-  padding: 20px;
   text-decoration: none;
 |]
 
 link location str = aStyle $ href location $ a [ text str ]
 
 headlineStyle = [style|
-  font-family: "Sagittarius A", "Sagittarius B";
   font-style: normal;
-  font-weight: 400;
-  color: #A5BF04;
+  color: #b76e79;
 |]
 
 titleStyle = [style|
-  font-size: 96px;
+  font-family: "Sagittarius A", "Sagittarius B";
+  font-size: 100px;
+  font-weight: 400;
   margin: 1rem;
   letter-spacing: 5px;
 |]
 
+alternateColors :: String -> [Purview event m]
+alternateColors str =
+  let
+    letterAndColor = zip str (cycle ["#c1db75", "#eebd53"])
+  in
+    fmap
+      (\(letter, color) -> istyle ("color: " <> color) $ span [ text [letter] ] )
+      letterAndColor
+
 title str
   = titleStyle
   $ headlineStyle
-  $ h1 [ text str ]
+  $ h1 str
 
 subtitleStyle = [style|
-  font-size: 36px;
+  font-size: 24px;
   margin: 1rem;
+  font-weight: 400;
 |]
 
 subtitle str
@@ -49,19 +60,34 @@ subtitle str
 
 nav = navStyle $ div
   [ link "/" "Home"
-  , link "/examples" "Examples"
   , link "/docs" "Docs"
+  , link "/examples" "Examples"
+  , link "/github" "Github"
   ]
 
-centerStyle = [style|
+topBoxStyle = [style|
   text-align: center;
+  padding-top: 10%;
 |]
+
+dividerStyle = [style|
+  border: 3px solid #b76e79;
+  background-color: #b76e79;
+  max-width: 15px;
+  height: 15px;
+  width: 15px;
+  border-radius: 25px;
+  margin: auto;
+  margin-bottom: 45px;
+|]
+
 
 topLevel = div
   [ nav
-  , centerStyle $ div
-      [ title "Purview"
-      , subtitle "Build server rendered interactive websites with Haskell"
+  , topBoxStyle $ div
+      [ title $ alternateColors "Purview"
+      -- , dividerStyle $ div []
+      , subtitle "Build server rendered, interactive websites with Haskell"
       ]
   ]
 
@@ -72,8 +98,10 @@ htmlHeadAdditions = [r|
     body {
       margin: 0;
       padding: 0;
-      font-family: sans-serif;
+      font-family: "Gotham SSm A", "Gotham SSm B", sans-serif;
       font-size: 18px;
+      max-width: 65rem;
+      margin: auto;
     }
   </style>
   <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6107252/6057832/css/fonts.css" />
